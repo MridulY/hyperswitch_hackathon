e2c81b661a2134693e469acb74941b9e25d64fb9
MridulY <mridulyadav3173@gmail.com>
2023-03-21 11:34:33 +0530

    Payment Authorize Capture Sync And Void

---
+++ crates/router/src/db/reverse_lookup.rs
@@ -0,0 +1,61 @@
+use error_stack::IntoReport;
+
+use super::{cache, MockDb, Store};
+use crate::{
+    connection,
+    errors::{self, CustomResult},
+    types::storage::reverse_lookup::{ReverseLookup, ReverseLookupNew},
+};
+
+#[async_trait::async_trait]
+pub trait ReverseLookupInterface {
+    async fn insert_reverse_lookup(
+        &self,
+        _new: ReverseLookupNew,
+    ) -> CustomResult<ReverseLookup, errors::StorageError>;
+    async fn get_lookup_by_lookup_id(
+        &self,
+        _id: &str,
+    ) -> CustomResult<ReverseLookup, errors::StorageError>;
+}
+
+#[async_trait::async_trait]
+impl ReverseLookupInterface for Store {
+    async fn insert_reverse_lookup(
+        &self,
+        new: ReverseLookupNew,
+    ) -> CustomResult<ReverseLookup, errors::StorageError> {
+        let conn = connection::pg_connection_write(self).await?;
+        new.insert(&conn).await.map_err(Into::into).into_report()
+    }
+
+    async fn get_lookup_by_lookup_id(
+        &self,
+        id: &str,
+    ) -> CustomResult<ReverseLookup, errors::StorageError> {
+        let database_call = || async {
+            let conn = connection::pg_connection_read(self).await?;
+            ReverseLookup::find_by_lookup_id(id, &conn)
+                .await
+                .map_err(Into::into)
+                .into_report()
+        };
+        cache::get_or_populate_cache(self, id, database_call).await
+    }
+}
+
+#[async_trait::async_trait]
+impl ReverseLookupInterface for MockDb {
+    async fn insert_reverse_lookup(
+        &self,
+        _new: ReverseLookupNew,
+    ) -> CustomResult<ReverseLookup, errors::StorageError> {
+        Err(errors::StorageError::MockDbError.into())
+    }
+    async fn get_lookup_by_lookup_id(
+        &self,
+        _id: &str,
+    ) -> CustomResult<ReverseLookup, errors::StorageError> {
+        Err(errors::StorageError::MockDbError.into())
+    }
+}
